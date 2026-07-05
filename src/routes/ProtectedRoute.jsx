29a0609c812c0161fe4ace6c0f40b8db58708fd0
Darshan <darshandv17@gmail.com>
2026-07-05 23:48:53 +0530

    Fix css filename case

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,4 +1,13 @@
+import Cookies from "js-cookie";
+import { Navigate } from "react-router-dom";
+
 function ProtectedRoute({ children }) {
+  const token = Cookies.get("jwt_token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
   return children;
 }
 
